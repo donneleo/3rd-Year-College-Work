866392d0983989fa9581dc2e22a0297cf4c9637e
Eoin Maguire <60257358+donneleo@users.noreply.github.com>
2020-12-07 17:42:37 +0000

    finished

--- CSU34011 Symbolic Programming/Assignment 2/Assignment-2.pl
+++ CSU34011 Symbolic Programming/Assignment 2/Assignment-2.pl
@@ -9,25 +9,18 @@ r(Num) --> [Num].
 
 %Question 2
 %Defines the neighbourhood, and the conditions to be met
-nbd --> h(Col1,Nat1,Pet1), h(Col2,Nat2,Pet2), h(Col3,Nat3,Pet3), 
+
+nbd --> h(Col1, Nat1, Pet1), h(Col2, Nat2, Pet2), h(Col3, Nat3, Pet3), 
     {Col1\=Col2, Col1\=Col3, Col2\=Col3, 
     Nat1\=Nat2, Nat1\=Nat3, Nat2\=Nat3, 
     Pet1\=Pet2, Pet1\=Pet3, Pet2\=Pet3}.
 %Defines a house
-h(Col, Nat, Pet) --> col(Col), nat(Nat), pet(Pet).
-
-%Conditions
-col(red) --> [red].
-col(blue) --> [blue].
-col(green) --> [green].
-
-nat(english) --> [english].
-nat(japanese) --> [japanese].
-nat(spanish) --> [spanish].
-
-pet(snail) --> [snail].
-pet(jaguar) --> [jaguar].
-pet(zebra) --> [zebra].
+h(Col, Nat, Pet) --> [h(Col, Nat, Pet)],
+    {
+        (Col = red; Col = blue; Col = green),
+      	(Nat = english; Nat = japanese; Nat = spanish),
+      	(Pet = snail; Pet = jaguar; Pet = zebra)
+    }.
 
 
 %Question 3
@@ -65,19 +58,20 @@ q4 --> [].
 
 %Part 3
 
-l3(String, Numeral) :- accept(String), numeral(Numeral).
-
 numeral(0).
 numeral(succ(X)) :- numeral(X).
 
+string_length([], 0).
+string_length([_|T], succ(X)) :- string_length(T, X), numeral(X).
+
+l3([1,0,0], succ(succ(succ(0)))).
+l3([1,0,1], succ(succ(succ(0)))).
+l3([1,1,0], succ(succ(succ(0)))).
+l3([1,1,1], succ(succ(succ(0)))).
 
 
-
-
-
-
-
-
+l3([0|T], succ(X)) :- string_length(T, X), X\=0, X\=succ(0), l3(T,X).
+l3([1|T], succ(X)) :- string_length(T, X), X\=0, X\=succ(0), l3(T,X).
     
     
 
